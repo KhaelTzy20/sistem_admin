37a86d98a77d7bfef19ab177c07a04e18074f7e1
godzhadow <godzhadow@gmail.com>
2026-05-12 16:16:07 +0700

    add softdelete in division model

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Division extends Model
 {
+    use HasFactory, SoftDeletes;
     protected $table = 'divisions';
 
     protected $fillable = ['name'];
